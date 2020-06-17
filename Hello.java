8207fd3d229aac3690a8d2160ad2c2ecf57649bf
sam402 <66077604+sam402@users.noreply.github.com>
2020-06-17 16:16:16 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package com.firstchoice.Github_Eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Second change from First Choice developer" );
+        System.out.println( "Hello World!" + " First change from First Choice" );
+    }
+}
